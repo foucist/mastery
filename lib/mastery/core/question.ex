875d21d630cbd467dbc583343bcebdfa3cd568f1
James Robey <james.robey@gmail.com>
2020-08-01 13:27:09 +0700

    Add functions for Response, Question, and Template

--- lib/mastery/core/question.ex
+++ lib/mastery/core/question.ex
@@ -1,12 +1,37 @@
 defmodule Mastery.Core.Question do
-  @moduledoc """
-  asked (String.t)
-    The question text for a user. For example, "1 + 2".
-  template (Template.t)
-    The template that created the question.
-  substitutions (%{ substitution: any})
-    The values chosen for each substitution field in a template. For example, for a template <%= left %> + <%= right %>, the substitutions might be %{ "left" => 1, "right" => 2}.
-  """
+  alias Mastery.Core.Template
 
   defstruct ~w[asked template substitutions]a
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      template: template,
+      substitutions: substitutions
+    }
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
 end
